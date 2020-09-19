e3e6762872d3725e15882cc81341cf5e6a14ebec
PaoloFrancesco-Marino <paolo.francesco.marino@gmail.com>
2020-09-19 12:32:35 +0200

    add icon + logout link

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import './HeaderComponent.css';
+
+import { FiUserPlus, FiLogOut } from "react-icons/fi";
 
 export default class HeaderComponent extends Component {
     render() {
         return ( 
             <div className="header-component d-flex align-items-center">
                 <div className="add">
-
+                <FiUserPlus className="nav-icon"/>
                 </div>
-                <div className="profile d-flex align-items-center">
+                <div className="profile d-flex align-items-center justify-content-center">
                     <img src="/images/profile.jpg" alt="Paolo"/>
                 </div>
                 <div className="logout">
-
+                    <Link to="/">
+                        <FiLogOut className="nav-icon"/>
+                    </Link>
                 </div>
             </div>
         )
